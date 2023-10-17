c8d06d8cee9d83e2b748b9f6a88d84232b298a56
DonaldHobson <donaldphobson@yahoo.co.uk>
2023-10-17 21:02:16 +0100

    Add files via upload

---
+++ main.js
@@ -0,0 +1,8 @@
+
+//alert("The test extension is up and running")
+
+//$("#dismiss-button")
+document.getElementById("dismiss-button").children[0]
+.children[0].children[1]
+.children[0].click();
+//alert("The test extension is up and running")
